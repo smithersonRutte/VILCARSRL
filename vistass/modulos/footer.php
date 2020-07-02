11455e7c5d5ed2acfccf02562f7e34a14e4d8e4c
Smith Rutte <65459806+smithersonRutte@users.noreply.github.com>
2020-07-02 16:37:29 -0500

    Add files via upload

---
+++ vistass/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2020 <a href="http://www.oya420.com/" target="_blank">Sergio Rutte Orosco</a>.</strong>
+
+	Todos los derechos reservados. --  ASIGNATURA: PROCESOS DE SOFTWARE
+
+
+</footer>
