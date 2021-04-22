82e08f019cfa3f866296b9b51368f660e6cded5a
BrayanJcr <70727399+BrayanJcr@users.noreply.github.com>
2021-04-22 12:21:35 -0500

    Prueba

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -4,7 +4,7 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<title><?php echo COMPANY; ?></title>
+	<title><?php echo Sistema; ?></title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<link rel="stylesheet" href="<?php echo SERVERURL; ?>vistas/css/main.css">
